588dc29c10d3520fc5a1075ff0478f0ede8a7576
Mobina Movahedi <mobina.mvd22@gmail.com>
2026-05-20 20:13:27 +0330

    1405.02.30 - First Commit

---
+++ android/settings.gradle.kts
@@ -0,0 +1,48 @@
+pluginManagement {
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        maven(url = "https://maven.myket.ir")
+        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://maven.aliyun.com/repository/public")
+
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
+    repositories {
+        // 🔥 مهم‌ترین خط (Flutter engine repo)
+        maven(url = "https://storage.googleapis.com/download.flutter.io")
+
+        // 🔥 mirrors برای dependencyهای اندروید
+        maven(url = "https://maven.myket.ir")
+        maven(url = "https://pub-azs.ir/api/mavens/")
+        maven(url = "https://maven.aliyun.com/repository/google")
+        maven(url = "https://maven.aliyun.com/repository/public")
+
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+}
+
+include(":app")
